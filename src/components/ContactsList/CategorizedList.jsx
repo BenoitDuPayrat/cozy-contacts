2fd79839309347c9af79196df5d04174964a2cdc
Benoît du Payrat <benoit@du-payrat.com>
2021-02-17 15:59:52 +0100

    Prettier-compliant code

--- src/components/ContactsList/CategorizedList.jsx
+++ src/components/ContactsList/CategorizedList.jsx
@@ -12,35 +12,36 @@ import { categorizeContacts } from '../../helpers/contactList'
 
 const CategorizedList = ({ contacts }) => {
   const { t } = useI18n()
-  const categorizedContacts = categorizeContacts(contacts, t('empty-list'));
+  const categorizedContacts = categorizeContacts(contacts, t('empty-list'))
 
-  /*  Je déclare un dictionnaire pour stocker les références vers les composants des catégories.
-  **  J'utilise un dictionnaire plutôt qu'un array de taille fixe car la liste de contacts peut changer
-  **  (entrer une recherche puis la supprimer permet de créer cette situation)
-  **  et il faut donc avoir une structure mutable, un dictionnaire étant plus flexible qu'un array.
-  */
-  const categoryRefs = useRef({}).current;
+  //  Je déclare un dictionnaire pour stocker les références vers les composants des catégories.
+  //  J'utilise un dictionnaire plutôt qu'un array de taille fixe car la liste de contacts peut changer
+  //  (entrer une recherche puis la supprimer permet de créer cette situation)
+  //  et il faut donc avoir une structure mutable, un dictionnaire étant plus flexible qu'un array.
+  const categoryRefs = useRef({}).current
   //  Cette fonction permet d'assigner les composants directement dans le dictionnaire au render.
-  const addCategory = (header) => ref => { categoryRefs[header] = ref };
+  const addCategory = header => ref => {
+    categoryRefs[header] = ref
+  }
 
   //  Cette fonction envoie vers le composant référencé par le header correspondant au bouton cliqué.
   function scrollToCategory(header) {
-    const ref = categoryRefs[header];
+    const ref = categoryRefs[header]
     if (ref) {
       ref.scrollIntoView({ behavior: 'smooth' })
     }
-  };
+  }
 
-  /*  J'ai utilisé les boutons définis par cozy-ui.
-  **  Je n'ai pas trouvé dans cozy-ui de composant de layout qui me satisfaisait pour le conteneur,
-  **  Donc j'ai créé un simple conteneur avec un fichier de style.
-  */
+  //  J'ai utilisé les boutons définis par cozy-ui.
+  //  Je n'ai pas trouvé dans cozy-ui de composant de layout qui me satisfaisait pour le conteneur,
+  //  Donc j'ai créé un simple conteneur avec un fichier de style.
   return (
     <>
       <div className="categories">
-        {Object.entries(categorizedContacts).map(([header, _]) => (
+        {Object.entries(categorizedContacts).map(([header]) => (
           <Button
-            onClick={_ => scrollToCategory(header)}
+            key={`cat-finder-${header}`}
+            onClick={() => scrollToCategory(header)}
             label={header}
             theme="secondary"
             extension="narrow"
